f80fdcb50d42fefb521643a826634de73ee5208f
TassyakurPasya <55564127+TassyakurPasya@users.noreply.github.com>
2019-09-23 13:44:11 +0700

    Add files via upload

---
+++ Biodata.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biodata;
+
+/**
+ *
+ * @author Lenovo
+ * Nama : Tassyakur Pasya
+ * Kelas : IF-01
+ * NIM : 10118009
+ * Matkul : PBO-01
+ */
+public class Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA" + "\n" + "--------------------------------");
+        System.out.println("Nama\t\t: Tassyakur Pasya");
+        System.out.println("Alamat\t\t: Jl. Budi No. 38,Cimahi");
+        System.out.println("Tempat Lahir\t: Jakarta");
+        System.out.println("Tanggal Lahir\t: 21 Januari 1999");
+        System.out.println("--------------------------------");
+        System.out.println("Hobi\t\t: Jalan-jalan");
+        System.out.println("Makan Kesukaan\t: Bakso");
+        System.out.println("Minuman Kesukaan: Jus Sirsak");
+    }
+    
+}
